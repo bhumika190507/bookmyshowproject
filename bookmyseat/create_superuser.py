11216cfa4466c3df1e701b2ad8977e65f19db36f
bhumika190507 <lalwanibhumika36@gmail.com>
2026-04-27 13:13:41 +0530

    Update create_superuser.py

--- bookmyseat/create_superuser.py
+++ bookmyseat/create_superuser.py
@@ -10,5 +10,5 @@ if not User.objects.filter(username="admin").exists():
     User.objects.create_superuser(
         username="admin",
         email="admin@gmail.com",
-        password="admin123"
+        password="soniya123"
     )
